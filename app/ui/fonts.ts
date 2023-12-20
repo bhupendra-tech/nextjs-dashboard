64e28830f9a3bd1ead730c19b1e1eb7da363b25b
Bhupendra <yashvaishnavxyz@gmail.com>
2023-12-20 15:20:38 +0530

    second commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+// file to manage fonts through out the app
+import { Inter } from 'next/font/google';
+import { Lusitana } from 'next/font/google'; 
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({weight : "400", subsets : ["latin"]})
